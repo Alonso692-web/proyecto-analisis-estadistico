30ae2a5696de74db89d8b9d3c1bbcb47a765a19c
Alonso <alonsolopezdominguez3@gmail.com>
2025-06-18 13:51:17 -0600

    Version usando gunicorn

--- app.py
+++ app.py
@@ -964,4 +964,5 @@ with open('templates/index.html', 'w', encoding='utf-8') as f:
     f.write(html_template.strip())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
